c2dedc5fcc311560d573e45c8984b61a5eb7b781
JuniorPD04 <167534141+JuniorPD04@users.noreply.github.com>
2024-07-03 00:11:26 -0500

    Update 4 - Triggers.sql

--- 4 - Triggers.sql
+++ 4 - Triggers.sql
@@ -29,8 +29,7 @@ BEGIN
       
         -- Calcular el costo total
         costo_total := dias_estancia * p_categoria;
-		
-        -- Aquí puedes realizar las acciones necesarias, como actualizar otra tabla con el costo total
+		-------------insert en detalle
 		INSERT INTO detalle_servicios(fecha_solicitud, hora_solicitud, descripcion_solicitud, 
 									 monto_servicio, transaccion_transaccion_id, servicio_servicio_id)
 		VALUES (new.fecha_salida, new.hora_salida, 'Se va mikin', costo_total, new.transaccion_id, 1);
@@ -41,12 +40,14 @@ BEGIN
 							  new.cliente_cliente_id) INTO nuevo_comprobante;
 	
 		IF nuevo_comprobante != -1 THEN
-			Insert into detalle_comprobante(comprobante_comprobante_id, servicio_servicio_id, monto)
-			SELECT nuevo_comprobante, ds.servicio_servicio_id, sum(monto_servicio)
-			FROM detalle_servicios ds
-			WHERE ds.transaccion_transaccion_id = 1
-			GROUP BY ds.servicio_servicio_id;
-		END IF;
+            -- Insertar en detalle_comprobante
+            INSERT INTO detalle_comprobante (comprobante_comprobante_id, servicio_servicio_id, monto)
+            SELECT nuevo_comprobante, ds.servicio_servicio_id, SUM(ds.monto_servicio)
+            FROM detalle_servicios ds
+            INNER JOIN transaccion tr ON tr.transaccion_id = ds.transaccion_transaccion_id
+            WHERE ds.transaccion_transaccion_id = NEW.transaccion_id
+            GROUP BY ds.servicio_servicio_id;
+        END IF;
     END IF;
     RETURN NEW;
 END;
@@ -54,3 +55,35 @@ $$ language 'plpgsql';
 
 Create or replace trigger tr_fn_actualizar_costo_total_y_tablas after update on transaccion
 	for each row execute procedure fn_actualizar_costo_total_y_tablas();
+
+---------------------------------------FUNCION_PARA CONSOLIDADO------------------------------
+CREATE OR REPLACE FUNCTION fn_obtener_consolidado(transaccion_id INTEGER)
+RETURNS NUMERIC AS $$
+DECLARE
+    consolidado NUMERIC(10,2);
+BEGIN
+    SELECT SUM(monto_servicio)
+    INTO consolidado
+    FROM detalle_servicios
+    WHERE transaccion_transaccion_id = transaccion_id
+    GROUP BY transaccion_transaccion_id;
+
+    RETURN consolidado;
+END;
+$$ LANGUAGE plpgsql;
+---------------------------------------------PA_DE_FRANQUE**********************
+
+CREATE OR REPLACE FUNCTION pa_insert_comprobante(trans_id int, tipo_comp CHAR(1), num_comp VARCHAR(10), monto NUMERIC(10,2), dni_emp CHAR(8), cli_cli_id int) RETURNS int AS $$
+DECLARE
+    retorno int;
+BEGIN
+    INSERT INTO comprobante (transaccion_transaccion_id, tipo_comprobante, numero_comprobante, monto_total, empleado_dni_empleado, cliente_cliente_id)
+    VALUES (trans_id, tipo_comp, num_comp, monto, dni_emp, cli_cli_id)
+    RETURNING comprobante_id INTO retorno;
+    RETURN retorno;
+EXCEPTION WHEN OTHERS THEN
+    RETURN -1;
+END;
+$$ LANGUAGE plpgsql;
+
+-----------------------------------
